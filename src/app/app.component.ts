f438f7dd798727e5545c03e9d30abbc1ce9d0145
Deepak Soni <Deepak.Soni@Incture.com>
2018-03-30 14:49:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Purchase Order'; //Header of application
+  headerDetails = { // header label
+       headerTitle : 'Header Info',
+       requester : "Requestor",
+       companyCode : "Company Code",
+       vendor : "Vendor",
+       materialGp : "Material Group",
+       purchseOrg : "Purchase Org",
+       currency : "Currency",
+       docType : "Document Type",
+       createdDate : "Created Date"     
+    };
+  lineItems = { //item level label
+         select : "Select",
+         material : "Material",
+         materialDescription : "Material Description",
+         plant : "Plant",
+         storage : "Storage",
+         unitPrice : "Unit Price",
+         quantity : "Quantity",
+         totalPrice : "Total Price"
+       }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReqHeaderComponent } from './req-header/req-header.component';
+import { ReqItemsComponent } from './req-items/req-items.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http';
+import { OdataComponent } from './odata/odata.component';
+ 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReqHeaderComponent,
+    ReqItemsComponent,
+    MainPageComponent,
+    OdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
